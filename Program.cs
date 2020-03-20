f8719a9dc24bbf1697094aa706bca25c9ad5d75e
Bárbara <babyr.oliveira@gmail.com>
2020-03-20 15:04:42 -0300

    Resolução do exercício proposto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Preparar...");
+            Console.ReadKey();
+            Console.WriteLine("Apontar...");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("FOGO!");
+            Console.ReadKey();
+        }
+    }
+}
